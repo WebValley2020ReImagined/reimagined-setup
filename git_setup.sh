8760696e6b887e1c069db7bb9f68c3c388dbdf3b
Luca Coviello <coviello@fbk.eu>
2020-08-22 10:55:24 +0200

    Update git configuration setup

--- git_setup.sh
+++ git_setup.sh
@@ -1,9 +1,11 @@
 ssh-keygen -t rsa -b 4096 -f /home/$USER/.ssh/github
 
-echo "Host github.com
-	Hostname github.com
-	User git
-	PreferredAuthentications publickey
-	IdentityFile /home/gabriele/.ssh/github" >> /home/$USER/.ssh/config
+# this configuration should already be present in your config file,
+# if not, execute the following command
+# echo "Host github.com
+# 	Hostname github.com
+# 	User git
+# 	PreferredAuthentications publickey
+# 	IdentityFile /home/gabriele/.ssh/github" >> /home/$USER/.ssh/config
 
 echo "remember to copy the content of /home/$USER/.ssh/github.pub when to your github profile!"
